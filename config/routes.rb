d7b896b1ce45ee2a4e5b3c2a6b04bee0ec2255ae
EC2 Default User <ec2-user@ip-172-31-26-216.ec2.internal>
2022-03-01 09:30:12 +0000

    [Update]管理者側の機能実装完了　残りはレイアウト調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+  devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+  namespace :admin do
+    resources :genres, only: [:index, :edit, :create, :update]
+    resources :items, only: [:index, :new, :show, :edit, :create, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    get '/' => "homes#top"
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
